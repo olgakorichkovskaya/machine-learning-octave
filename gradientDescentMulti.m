fdc0548fbf73db2e87fbf751b9809ab2d1a70feb
Dewy <olgakorichkovskaya@gmail.com>
2015-07-01 19:52:51 +0300

    logistic regression

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -12,25 +12,21 @@ t_history = zeros(num_iters, n);
 t=zeros(n,1);
 for iter = 1:num_iters
 for i=1:n
-    sum=sum1(X, y, theta,m, i,n);
-t(i,1)=theta(i,1)-alpha*sum;
+  %  sum=sum1(X, y, theta,m, i,n);
+   
+t(i,1)=theta(i,1)-alpha*sum( (X*theta-y)'*X(:,i))/m;
 end
 theta=t;
     % Save the cost J in every iteration    
     curent_J= computeCostMulti(X, y, theta);
     J_history(iter) =  curent_J;
     
+    
 t_history(iter,:) = theta(:,1);
-%if(curent_J< best_j)
-%    best_j = J_history(iter);
-%    best_t=t;
-%end
 
 end
 plotData1( 1:num_iters, J_history,'iteration','J');
-%plotData2Dim( 1:num_iters, t_history(:,1), t_history(:,2), 'iteration','theta 0','theta 1');
 
-%theta=best_t;
 tt= computeCost(X, y, theta); 
 tml='Best J %.3f \n';
 fprintf(tml,tt);
