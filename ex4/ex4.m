9da3dada4f1bbbb336706586e5b0743eb3014ca2
Dewy <olgakorichkovskaya@gmail.com>
2015-07-14 13:10:51 +0300

    NN: backfarward

--- ex4/ex4.m
+++ ex4/ex4.m
@@ -1,35 +1,12 @@
-%% Machine Learning Online Class - Exercise 4 Neural Network Learning
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     sigmoidGradient.m
-%     randInitializeWeights.m
-%     nnCostFunction.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
-%% Initialization
+%% Machine Learning 4 Neural Network Learning
 clear ; close all; clc
 
-%% Setup the parameters you will use for this exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 hidden_layer_size = 25;   % 25 hidden units
 num_labels = 10;          % 10 labels, from 1 to 10   
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
-%  You will be working with a dataset that contains handwritten digits.
-%
-
-% Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
 load('ex4data1.mat');
@@ -39,16 +16,9 @@ m = size(X, 1);
 sel = randperm(size(X, 1));
 sel = sel(1:100);
 
-displayData(X(sel, :));
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+%displayData(X(sel, :));
 
 %% ================ Part 2: Loading Parameters ================
-% In this part of the exercise, we load some pre-initialized 
-% neural network parameters.
-
 fprintf('\nLoading Saved Neural Network Parameters ...\n')
 
 % Load the weights into variables Theta1 and Theta2
--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -22,7 +22,6 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-% Setup some useful variables
 m = size(X, 1);
          
 % You need to return the following variables correctly 
@@ -60,32 +59,50 @@ Theta2_grad = zeros(size(Theta2));
 %               backpropagation. That is, you can compute the gradients for
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
-%
+%m = the number of training examples
+%n = the number of training features, including the initial bias unit.
+%h = the number of units in the hidden layer - NOT including the bias unit
+%r = the number of output classifications 10
 
 
+% a3, a2, z2, and a1 
+a1=[ones(m, 1) X];
+z2=a1 * Theta1'; % (m x n) ? (n x h) --> (m x h) 5000x25
+a2 = sigmoid(z2);% 5000x25
+a2=[ones(m, 1) a2];%??
+a3 = sigmoid(a2 * Theta2');
+%[t, p] = max(a3, [], 2);
 
 
+K= num_labels;
+y_mat = eye(num_labels);
+y1 = y_mat(y,:);
+sum=0;
+for i=1:m
+ for k=1:K
+     add1=-y1(i,k)*log(a3(i,k));
+     add2=(1-y1(i,k))*log(1-a3(i,k));
+ sum=sum+add1-add2;
+ end
+end
+J=sum/m;
+
+d3=a3-y1;% mxr 5000x10
+
+% d2 is tricky. It uses the (:,2:end) columns of Theta2. d2 is the product 
+%of d3 and Theta2(no bias),
+%then element-wise scaled by sigmoid gradient of z2. 
+d2=d3*Theta2(:,2:end); % (m x r) ? (r x h) --> (m x h) 5000x25
+
+% Delta1 is the product of d2 and a1
+Delta1=d2'*a1; % (h x m) ? (m x n) --> (h x n) 25x 401
+Delta2=d3'*a2;  % (r x m) ? (m x [h+1]) --> (r x [h+1]) 10 x 26
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
+Theta1_grad= Delta1/m;   
+Theta2_grad= Delta2/m;   
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
-
 end
+
