f0c4be4bf5a0b64194046d4308d82de13b0f2a3d
Dewy <olgakorichkovskaya@gmail.com>
2015-07-30 16:06:25 +0300

    SVM and spam filter

--- ex6/ex6_spam.m
+++ ex6/ex6_spam.m
@@ -1,31 +1,6 @@
-%% Machine Learning Online Class
-%  Exercise 6 | Spam Classification with SVMs
-%
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     gaussianKernel.m
-%     dataset3Params.m
-%     processEmail.m
-%     emailFeatures.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
-%% Initialization
 clear ; close all; clc
 
 %% ==================== Part 1: Email Preprocessing ====================
-%  To use an SVM to classify emails into Spam v.s. Non-Spam, you first need
-%  to convert each email into a vector of features. In this part, you will
-%  implement the preprocessing steps for each email. You should
-%  complete the code in processEmail.m to produce a word indices vector
-%  for a given email.
-
 fprintf('\nPreprocessing sample email (emailSample1.txt)\n');
 
 % Extract Features
@@ -41,10 +16,7 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ==================== Part 2: Feature Extraction ====================
-%  Now, you will convert each email into a vector of features in R^n. 
-%  You should complete the code in emailFeatures.m to produce a feature
-%  vector for a given email.
-
+%  convert each email into a vector of features in R^n. 
 fprintf('\nExtracting features from sample email (emailSample1.txt)\n');
 
 % Extract Features
@@ -60,7 +32,7 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% =========== Part 3: Train Linear SVM for Spam Classification ========
-%  In this section, you will train a linear classifier to determine if an
+%  train a linear classifier to determine if an
 %  email is Spam or Not-Spam.
 
 % Load the Spam Email dataset
